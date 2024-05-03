8add86a3defc920087b82555873befe58fbf0810
iamfiham <xyzfiham@gmail.com>
2024-05-03 22:26:39 +0530

    bug fix

--- src/componants/DeleteModel.jsx
+++ src/componants/DeleteModel.jsx
@@ -3,10 +3,10 @@ import {RiDeleteBinLine} from 'react-icons/ri';
 const DeleteModel = ({id, setIsDeleteModelOpen, deleteNote}) => {
   return (
     <div className='fixed top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.35)] grid place-items-center z-[200]'>
-      <div className='flex flex-col items-center gap-3 p-6 bg-neutral-50 rounded-lg shadow-lg w-[300px] border border-solid border-neutral-200'>
-        <RiDeleteBinLine className='text-5xl fill-red-400 mb-3' />
-        <h2 className='text-base font-semibold leading-none '>Delete Note</h2>
-        <h4 className='text-xs font-normal leading-[1.25] text-center text-neutral-400 mb-3'>
+      <div className='flex flex-col items-center p-6 bg-neutral-50 rounded-lg shadow-lg w-[300px] border border-solid border-neutral-200'>
+        <RiDeleteBinLine className='text-5xl fill-red-400 my-6' />
+        <h2 className='text-base font-semibold leading-none mb-3'>Delete Note</h2>
+        <h4 className='text-xs font-normal leading-[1.25] text-center text-neutral-400 mb-6'>
           Are you sure you want to delete this note? <br /> This action cannot be undone
         </h4>
         <div className='grid w-full gap-2'>
